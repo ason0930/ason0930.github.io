38c725e25a867bb9ba2a096908fe75d95b691292
ason0930 <42229855+ason0930@users.noreply.github.com>
2020-05-27 16:09:53 +0800

    Add files via upload

---
+++ portfolio/js/modal_images.js
@@ -0,0 +1,17 @@
+// Get the button that open the modal
+function myBtnFunction(numA) {
+  var x, i;
+  x = document.querySelectorAll(".ason-modal");
+  for (i = 0; i < x.length; i++) {
+    return x[numA].style.display = "block";
+  }
+}
+
+// Get the button that close the modal
+function myCloseFunction() {
+  var x, i;
+  x = document.querySelectorAll(".ason-modal");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+}

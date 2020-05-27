b6495c72db23f17c4945736aed59d7f4b5ee5b4b
Ason <ason0930@gmail.com>
2020-05-27 16:52:54 +0800

    delete all

--- portfolio/js/btn_home.js
+++
@@ -1,2 +0,0 @@
-TweenMax.from(".js-btn-home", 0.5, { ease: Power2.easeOut, x: -100, opacity: 0, scale: 1 });
-TweenMax.from(".js-btn-home-xs", 1, { ease: Power2.easeOut, x: -100, opacity: 0, scale: 1 });
